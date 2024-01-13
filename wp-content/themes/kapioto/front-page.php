a4f2deefe8e52a34a7d0e409f1608e74ef0276e9
Arlind Ramadani <arlind.ramadani@gig-ext.com>
2024-01-13 18:45:05 +0100

    Implementation of banner

--- wp-content/themes/kapioto/front-page.php
+++ wp-content/themes/kapioto/front-page.php
@@ -4,5 +4,30 @@
 */
 get_header(); 
 ?>
-    Front-page
+
+<div class="banner-container">
+    <div class="container">
+        <div class="banner-wrapper">
+            <div class="content">
+                <h3>Business theme</h3>
+                <h2>We Grow Up your Business</h2>
+
+                <p>we are a new bipital Apency basediin USA. We nave overto years of combined experience, and know a thing or two about designing websites and mobile apps</p>
+
+                <div class="actions-wrapper">
+                    <a href="#" class="btn btn-primary">Get in touch</a>
+                    <a href="#" class="btn btn-secondary">Buy theme</a>
+                </div>
+            </div>
+
+            <div class="image-wrapper">
+                <span>
+                    <div class="play-button"></div>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <?php get_footer(); ?>
